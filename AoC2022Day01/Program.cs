0cee420665f65aee69e0121b9b118cb731d005fd
Lukas.Bijelic <Lukas.Bijelic@htlvb.at>
2022-12-12 09:57:54 +0100

    Read input file

--- AoC2022Day01/Program.cs
+++ AoC2022Day01/Program.cs
@@ -1,21 +1,2 @@
-﻿string sample = @"1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000";
-
-int maxCalories = sample.Split("\r\n\r\n")
-    .Max(elf =>
-    elf.Split("\r\n")
-    .Sum(int.Parse)
-    );
-Console.WriteLine($"Part 1: {maxCalories}");
+﻿string input = File.ReadAllText("input01.txt");
+Console.WriteLine(input);
